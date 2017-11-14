8436841b484eb72b5b362b5af23850f7ae09dd42
John Wu <johnwu93@gmail.com>
2017-11-14 12:54:00 -0800

    refactor: Encapsulated functions into player
    I did not want to expose some methods of player so I used the module
    pattern to hide it's contents.

--- js/models/player.js
+++ js/models/player.js
@@ -1,94 +1,130 @@
-var MOVES = {
-  LEFT: 'left',
-  RIGHT: 'right',
-  UP: 'up',
-  DOWN: 'down'
-};
+var Player = (function () {
 
-function getNumRows() {
-  return BOARD.canvas.length;
-}
-
-function getNumCols() {
-  return BOARD.canvas[0].row.length;
-}
-
-var Player = function (rowId, colId, sprite) {
-  'use strict';
-  RenderableUnit.call(this);
-  this.moveRequest = null;
-  this.rowId = rowId;
-  this.colId = colId;
-  this.sprite = sprite;
-};
-
-Player.prototype = Object.create(RenderableUnit.prototype);
-
-Player.prototype.handleInput = function (moveCommand) {
-  'use strict';
-  this.moveRequest = moveCommand;
-};
-
-Player.prototype.bindKeyBoard = function bindKeyBoard() {
-  'use strict';
-  var player = this;
-  var allowedKeys = {
-    37: MOVES.LEFT,
-    38: MOVES.UP,
-    39: MOVES.RIGHT,
-    40: MOVES.DOWN
+  var myMOVES = {
+    LEFT: 'left',
+    RIGHT: 'right',
+    UP: 'up',
+    DOWN: 'down'
   };
-  document.addEventListener('keyup', function (e) {
-    player.handleInput(allowedKeys[e.keyCode]);
-  });
 
-};
+  var myRowId, myColId;
 
-Player.prototype.update = function update() {
-  'use strict';
-  if (this.moveRequest !== null) {
-    this.move(this.moveRequest);
+
+  // noinspection NestedFunctionJS
+  function Player(rowId, colId, sprite) {
+    'use strict';
+    RenderableUnit.call(this);
     this.moveRequest = null;
+    myRowId = rowId;
+    myColId = colId;
+    this.sprite = sprite;
   }
-};
 
-Player.prototype.move = function move(command) {
-  'use strict';
-  switch (command) {
-    case MOVES.LEFT:
-      if (this.colId > 0) {
-        this.colId = this.colId - 1;
-      }
-      break;
-    case MOVES.RIGHT:
-      if (this.colId < getNumCols() - 1) {
-        this.colId = this.colId + 1;
-      }
-      break;
-    case MOVES.UP:
-      if (this.rowId > 0) {
-        this.rowId = this.rowId - 1;
-      }
-      break;
-    case MOVES.DOWN:
-      if (this.rowId < getNumRows() - 1) {
-        this.rowId = this.rowId + 1;
-      }
-      break;
-    default:
-      throw Error('Invalid Move');
-      break;
+  // noinspection NestedFunctionJS
+  function getBoardNumRows() {
+    return BOARD.canvas.length;
   }
-};
 
-Player.prototype.getRenderData = function () {
-  'use strict';
-  var blockOffset = computeBlockRowPosition(this.rowId);
-  var rowPosition = blockOffset + BOARD.canvas[this.rowId].alignRowCenter(this.sprite);
-  var colPosition = computeBlockColumnPosition(this.colId);
-  return {
-    imagePath: this.sprite.imagePath,
-    position: new Position(colPosition, rowPosition)
+  // noinspection NestedFunctionJS
+  function getBoardNumCols() {
+    return BOARD.canvas[0].row.length;
+  }
+
+
+  Player.prototype = Object.create(RenderableUnit.prototype);
+
+  Player.prototype.getRow = function () {
+    'use strict';
+    return myRowId;
   };
 
-};
+  Player.prototype.getColumn = function () {
+    'use strict';
+    return myColId;
+  };
+
+  Player.prototype.setRow = function (rowId) {
+    myRowId = rowId;
+  };
+
+  Player.prototype.setColumn = function (colId) {
+    myColId = colId;
+  };
+
+  Player.prototype.handleInput = function (moveCommand) {
+    'use strict';
+    this.moveRequest = moveCommand;
+  };
+
+  Player.prototype.bindKeyBoard = function bindKeyBoard() {
+    'use strict';
+    var player = this;
+    var allowedKeys = {
+      37: myMOVES.LEFT,
+      38: myMOVES.UP,
+      39: myMOVES.RIGHT,
+      40: myMOVES.DOWN
+    };
+    document.addEventListener('keyup', function (e) {
+      player.handleInput(allowedKeys[e.keyCode]);
+    });
+
+  };
+
+  Player.prototype.update = function update() {
+    'use strict';
+    if (this.moveRequest !== null) {
+      this.move(this.moveRequest);
+      this.moveRequest = null;
+    }
+  };
+
+  Player.prototype.move = function move(command) {
+    'use strict';
+    var col, row;
+    switch (command) {
+      case myMOVES.LEFT:
+        col = this.getColumn();
+        if (col > 0) {
+          this.setColumn(col - 1);
+        }
+        break;
+      case myMOVES.RIGHT:
+        col = this.getColumn();
+        if (col < getBoardNumCols() - 1) {
+          this.setColumn(this.getColumn() + 1);
+        }
+        break;
+      case myMOVES.UP:
+        row = this.getRow();
+        if (row > 0) {
+          this.setRow(row - 1);
+        }
+        break;
+      case myMOVES.DOWN:
+        row = this.getRow();
+        if (row < getBoardNumRows() - 1) {
+          this.setRow(row + 1);
+
+        }
+        break;
+      default:
+        throw Error('Invalid Move');
+        break;
+    }
+  };
+
+  Player.prototype.getRenderData = function () {
+    'use strict';
+    console.log(this.getRow());
+    var blockOffset = computeBlockRowPosition(this.getRow());
+    var rowPosition = blockOffset + BOARD.canvas[this.getRow()].alignRowCenter(this.sprite);
+    var colPosition = computeBlockColumnPosition(this.getColumn());
+    return {
+      imagePath: this.sprite.imagePath,
+      position: new Position(colPosition, rowPosition)
+    };
+
+  };
+  return Player;
+})();
